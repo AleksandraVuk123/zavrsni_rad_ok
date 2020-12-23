fd150e54419297dfe567e5bc4a2e418a2a3151db
Aleksandra <azobenica@gmail.com>
2020-12-23 19:25:06 +0100

    zadatak4

--- index.php
+++ index.php
@@ -16,26 +16,31 @@
 
 <body>
 
-<?php include'header.php'; ?>
+<?php include 'header.php'; ?>
 
 <main role="main" class="container">
-    
+
     <div class="row">
+    
         <div class="col-sm-8 blog-main">
 
-            <?php include'posts.php'; ?>
-            <?php include'sidebar.php'; ?>
-
+        <?php include'posts.php'; ?>
+            
             <nav class="blog-pagination">
                 <a class="btn btn-outline-primary" href="#">Older</a>
                 <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
             </nav>
+
         </div><!-- /.blog-main -->
-       
+        
+        <?php include 'sidebar.php'; ?>  
+    
     </div><!-- /.row -->
 
 </main><!-- /.container -->
+
 <?php include'footer.php'; ?>
+
 </body>
 </html>
 
