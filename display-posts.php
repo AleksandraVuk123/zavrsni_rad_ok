d646c4fd2d5403bd970bebc23fef7df055981ddb
Aleksandra <azobenica@gmail.com>
2020-12-25 18:35:30 +0100

    zadatak5

--- display-posts.php
+++ display-posts.php
@@ -1,8 +1,13 @@
 <?php
     foreach ($results as $blog) { ?>
         <div class="blog-post">
+            
             <h2 class="blog-post-title"><a href="single-post.php?index=<?php echo $blog['id'] ?>"><?php echo $blog['title']; ?></a></h2>
             <p class="blog-post-meta"><?php echo $blog['created_at']; ?> by <a href="#"><?php echo $blog['author']; ?></a></p>
             <p><?php echo $blog['body'] ?></p>
-        </div><!-- /.blog-post -->
+
+        </div>
     <?php }
+
+
+
